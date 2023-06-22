eba519c4abc7f3822f6ae84204ee7b5716bd6129
Hayam Itani <hayam.itani@outlook.com>
2023-06-22 11:54:44 +0300

    Add files via upload

---
+++ src/app/pipe/translate.pipe.ts
@@ -0,0 +1,25 @@
+// src/app/translate.pipe.ts
+
+import { Pipe, PipeTransform } from '@angular/core';
+
+import { TranslateService } from '../services/translate.service';
+
+@Pipe({
+  name: 'translate',
+  pure: false
+})
+export class TranslatePipe implements PipeTransform {
+
+  constructor(private translate: TranslateService) {
+    
+  }
+
+  transform(key: any): any {
+    return this.translate.data[key] || key;
+
+}
+
+}
+
+
+
